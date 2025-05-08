0c6b5cc5eb11aed4e30bc894d82d0dcc62d39ee0
sourabh7847 <flank.valish@gmail.com>
2025-05-08 20:46:57 +0530

    Updates

---
+++ assets/with-props-yZZmM4Op.js
@@ -0,0 +1,1 @@
+import{q as p,s as t,t as e,v as o,r as s,w as u}from"./chunk-BAXFHI7N-Bz_EcWvQ.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
