148ca7f21b563fe11bcdcc7cfef67ab12c006f1a
sourabh7847 <flank.valish@gmail.com>
2025-05-08 20:56:28 +0530

    trying to host on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/productivity-tracker/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
